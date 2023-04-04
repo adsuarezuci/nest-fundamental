17ab107603777bbd02a23208e71935295cbbfd2e
adsuarezuci <adsuarezuci@gmail.com>
2023-04-04 06:58:33 -0400

    paras in a route

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
   findAll(): string {
     return 'Probando getAll coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param() params): string {
+    return `Se busco el dato por el id ${params.id}`;
   }
 }
