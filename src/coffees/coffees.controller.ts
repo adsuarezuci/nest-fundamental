8edcfd5addec1eb086f5b481f87981f84754d2b9
adsuarezuci <adsuarezuci@gmail.com>
2023-04-04 09:39:06 -0400

    crud and DTO

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,14 +1,45 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Patch,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Coffee } from './entities/coffee.entity';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeesAll: CoffeesService) {}
+
   @Get()
-  findAll(): string {
-    return 'Probando getAll coffees';
+  findAll(@Query() pagination): Coffee[] {
+    // const { limit, offset } = pagination;
+    return this.coffeesAll.findAll();
   }
 
   @Get(':id')
-  findOne(@Param() params): string {
-    return `Se busco el dato por el id ${params.id}`;
+  findOne(@Param() params): Coffee {
+    return this.coffeesAll.findOne(params.id);
+  }
+
+  @Post()
+  create(@Body() createCoffeeDTO: CreateCoffeeDto) {
+    this.coffeesAll.create(createCoffeeDTO);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCoffeeDTO: UpdateCoffeeDto) {
+    this.coffeesAll.update(id, updateCoffeeDTO);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    this.coffeesAll.remove(id);
   }
 }
